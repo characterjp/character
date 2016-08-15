6175a1d2d858aadc48d4850f14fc11d9a85e606a
okihouse <okihouse@brace.xyz>
2016-08-15 16:58:16 +0900

    join update

--- join/custom.js
+++ join/custom.js
@@ -1,24 +1,28 @@
 $(document).ready(function () {
-  
+
     //Wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var $target = $(e.target);
-    
+
         if ($target.parent().hasClass('disabled')) {
             return false;
         }
-    });
-
-    $(".btn-primary").click(function (e) {
-
-        var $active = $('.wizard .nav-wizard li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-
     });
 });
 
 function nextTab(elem) {
     $(elem).next().find('a[data-toggle="tab"]').click();
+}
+
+function now() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return year + '年' + month + '月' + day + '日'
+    //return [year, month, day].join('-');
 }
