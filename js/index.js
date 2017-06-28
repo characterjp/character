d0bf00f9e98a67500e2724d2f35b876002876260
ted <ted@listensoft.net>
2017-06-28 18:48:35 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -47,11 +47,11 @@
 			$("h3#s2").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
 			$("h3#s3").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
 			$("h3#s4").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
-			$("h1#s5").fitText(1,{ minFontSize: '40px', maxFontSize: '60px' });
-			$("span#s6").fitText(2,{ minFontSize: '30px', maxFontSize: '60px' });
-			$("h3#s6").fitText(1.8,{ minFontSize: '18px', maxFontSize: '40px' });
+			$("h1#s5").fitText(1.2,{ minFontSize: '40px', maxFontSize: '60px' });
+			$("span#s6").fitText(1,{ minFontSize: '25px', maxFontSize: '45px' });
+			$("h3#s6").fitText(1.8,{ minFontSize: '18px', maxFontSize: '35px' });
 			$("h3#s7").fitText(1,{ minFontSize: '20px', maxFontSize: '35px' });
-			$("span#moveBtn").fitText(1,{ minFontSize: '15px', maxFontSize: '30px' });
+
 
 		});	//document.ready End
 
